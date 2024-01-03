644266c9feb2822547ca83bd04e910898bc63701
Lunar <nightsky5070@gmail.com>
2024-01-04 00:48:02 +0900

    test: Testing Logic

--- src/events/chzzk/checkChzzkLive.ts
+++ src/events/chzzk/checkChzzkLive.ts
@@ -21,80 +21,82 @@ export async function CheckChzzkLive({
             // check time
             notificationConfig.lastChecked = new Date();
 
-            if (
-                lastOpenDate === null ||
-                (new Date(liveDetail.openDate) > new Date(lastOpenDate))
-            ) {
-                const targetGuild =
-                    client.guilds.cache.get(notificationConfig.guildId) ||
-                    (await client.guilds.fetch(notificationConfig.guildId));
-                if (!targetGuild) {
-                    await ChzzkLiveModel.findOneAndDelete({_id: notificationConfig._id});
-                    continue;
-                }
+            if (liveDetail) {
+                if (
+                    lastOpenDate === null ||
+                    (new Date(liveDetail.openDate) > new Date(lastOpenDate))
+                ) {
+                    const targetGuild =
+                        client.guilds.cache.get(notificationConfig.guildId) ||
+                        (await client.guilds.fetch(notificationConfig.guildId));
+                    if (!targetGuild) {
+                        await ChzzkLiveModel.findOneAndDelete({_id: notificationConfig._id});
+                        continue;
+                    }
 
-                const targetChannel =
-                    targetGuild.channels.cache.get(notificationConfig.channelId) as TextChannel
-                    || (await targetGuild.channels.fetch(notificationConfig.channelId) as TextChannel);
-                if (!targetChannel) {
-                    await ChzzkLiveModel.findOneAndDelete({_id: notificationConfig._id});
-                    continue;
-                }
+                    const targetChannel =
+                        targetGuild.channels.cache.get(notificationConfig.channelId) as TextChannel
+                        || (await targetGuild.channels.fetch(notificationConfig.channelId) as TextChannel);
+                    if (!targetChannel) {
+                        await ChzzkLiveModel.findOneAndDelete({_id: notificationConfig._id});
+                        continue;
+                    }
 
-                notificationConfig.openDate = new Date(liveDetail.openDate + 'Z');
-                if (notificationConfig.liveId !== liveDetail.liveId) {
-                    notificationConfig.liveId = liveDetail.liveId;
-                }
+                    notificationConfig.openDate = new Date(liveDetail.openDate + 'Z');
+                    if (notificationConfig.liveId !== liveDetail.liveId) {
+                        notificationConfig.liveId = liveDetail.liveId;
+                    }
 
-                const streamLiveStatus = liveDetail.status;
+                    const streamLiveStatus = liveDetail.status;
 
-                if (streamLiveStatus === 'OPEN') {
-                    const channelName = liveDetail.channel.channelName;
-                    const channelLink = `https://chzzk.naver.com/${chzzkId}`
-                    const channelIconUrl = liveDetail.channel.channelImageUrl!;
+                    if (streamLiveStatus === 'OPEN') {
+                        const channelName = liveDetail.channel.channelName;
+                        const channelLink = `https://chzzk.naver.com/${chzzkId}`
+                        const channelIconUrl = liveDetail.channel.channelImageUrl!;
 
-                    const previewData = liveDetail.liveImageUrl.replace('_{type}', '_1080');
+                        const previewData = liveDetail.liveImageUrl.replace('_{type}', '_1080');
 
-                    const streamTitle = liveDetail.liveTitle
-                    const streamLink = `https://chzzk.naver.com/live/${chzzkId}`
-                    const streamCategory = liveDetail.liveCategoryValue
+                        const streamTitle = liveDetail.liveTitle
+                        const streamLink = `https://chzzk.naver.com/live/${chzzkId}`
+                        const streamCategory = liveDetail.liveCategoryValue
 
-                    notificationConfig
-                        .save()
-                        .then(async () => {
-                            const ChzzkLiveCard = new EmbedBuilder()
-                                .setColor('#00FFA3')
-                                .setTitle(streamTitle)
-                                .setURL(streamLink)
-                                .setDescription(`${channelName}님이 치지직 방송을 시작했습니다!`)
-                                .setAuthor({
-                                    name: channelName,
-                                    url: channelLink,
-                                    iconURL: channelIconUrl,
-                                })
-                                .setImage(previewData)
-                                .setFooter({text: 'Chzzk Live BETA with 새참이'})
-                                .setTimestamp();
+                        notificationConfig
+                            .save()
+                            .then(async () => {
+                                const ChzzkLiveCard = new EmbedBuilder()
+                                    .setColor('#00FFA3')
+                                    .setTitle(streamTitle)
+                                    .setURL(streamLink)
+                                    .setDescription(`${channelName}님이 치지직 방송을 시작했습니다!`)
+                                    .setAuthor({
+                                        name: channelName,
+                                        url: channelLink,
+                                        iconURL: channelIconUrl,
+                                    })
+                                    .setImage(previewData)
+                                    .setFooter({text: 'Chzzk Live BETA with 새참이'})
+                                    .setTimestamp();
 
-                            if (streamCategory) {
-                                ChzzkLiveCard.addFields([{name: '카테고리', value: `${streamCategory}`, inline: true}]);
-                            }
+                                if (streamCategory) {
+                                    ChzzkLiveCard.addFields([{name: '카테고리', value: `${streamCategory}`, inline: true}]);
+                                }
 
-                            if (notificationConfig.message !== null) {
-                                await targetChannel.send({
-                                    content: `${notificationConfig.message}`,
-                                    embeds: [ChzzkLiveCard],
-                                    allowedMentions: {
-                                        parse: ["everyone"]
-                                    }
-                                });
-                            } else {
-                                await targetChannel.send({
-                                    embeds: [ChzzkLiveCard],
-                                });
-                            }
-                        })
-                        .catch((e) => null);
+                                if (notificationConfig.message !== null) {
+                                    await targetChannel.send({
+                                        content: `${notificationConfig.message}`,
+                                        embeds: [ChzzkLiveCard],
+                                        allowedMentions: {
+                                            parse: ["everyone"]
+                                        }
+                                    });
+                                } else {
+                                    await targetChannel.send({
+                                        embeds: [ChzzkLiveCard],
+                                    });
+                                }
+                            })
+                            .catch((e) => null);
+                    }
                 }
             }
         }
